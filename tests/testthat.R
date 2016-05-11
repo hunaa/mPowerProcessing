229e4cb68b1aec07bb04d2c87a5ffcde99c0b9f6
bhoff <bruce.hoff@sagebase.org>
2016-05-11 12:23:55 -0700

    merged 'testthat' files back in to one

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,10 +7,4 @@ library(testthat)
 library("mPowerProcessing")
 
 # https://github.com/travis-ci/travis-ci/issues/3849
-test_check("mPowerProcessing", filter="_0")
-test_check("mPowerProcessing", filter="_1")
-test_check("mPowerProcessing", filter="_2")
-test_check("mPowerProcessing", filter="_3")
-test_check("mPowerProcessing", filter="_7")
-test_check("mPowerProcessing", filter="_8")
-test_check("mPowerProcessing", filter="_unit")
+test_check("mPowerProcessing")
